d25f62409b42fa32bd681a86c29cdd48559191bf
Varga Botond <btnd.varga@gmail.com>
2025-12-15 08:06:20 +0100

    Base Stuff

---
+++ react-app/api/lies.js
@@ -0,0 +1,11 @@
+/** POST /api/lies */
+export default async function handler(req, res) {
+    const method = req.method
+    if (method=='POST') {
+        const body = req.body
+        console.log('body', body)
+        return res.sendStatus(201)
+    } else{
+
+    }
+}
